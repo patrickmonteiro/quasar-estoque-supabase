0d7bd8cb26eca4870950b3f1d102a2321da90ee4
Patrick <eng.patrickmonteiro@gmail.com>
2022-02-27 22:49:48 -0300

    update title

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -3,7 +3,7 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>
-          Quasar Estoque
+          Quasar Estoque Supabase
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
