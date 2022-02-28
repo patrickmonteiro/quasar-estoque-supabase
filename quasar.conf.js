f521351fd0e0501521c0a7ce0918324565963402
Patrick <eng.patrickmonteiro@gmail.com>
2022-02-27 22:19:59 -0300

    add pwa mode

--- quasar.conf.js
+++ quasar.conf.js
@@ -151,7 +151,7 @@ module.exports = configure(function (ctx) {
     // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
     pwa: {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
-      workboxOptions: {}, // only for GenerateSW
+      workboxOptions: { skipWaiting: true, clientsClaim: true }, // only for GenerateSW
 
       // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
       // if using workbox in InjectManifest mode
